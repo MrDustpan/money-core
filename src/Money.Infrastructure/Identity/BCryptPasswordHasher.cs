f2621c55fad9fc3bd1652f7555180580e7748c45
Dustin Brown <mileslinus@gmail.com>
2016-11-02 22:05:40 -0500

    password validator

--- src/Money.Infrastructure/Identity/BCryptPasswordHasher.cs
+++ src/Money.Infrastructure/Identity/BCryptPasswordHasher.cs
@@ -2,11 +2,16 @@ using Money.Domain.Identity.Register;
 
 namespace Money.Infrastructure.Identity
 {
-  public class BCryptPasswordHasher : IPasswordHasher
+  public class BCryptPasswordHasher : IPasswordHasher, IPasswordValidator
   {
     public string Hash(string password)
     {
       return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool IsValid(string clearTextPassord, string hashedPassword)
+    {
+      return BCrypt.Net.Bcrypt.Verify(clearTextPassword, hashedPassword);
     }
   }
 }
